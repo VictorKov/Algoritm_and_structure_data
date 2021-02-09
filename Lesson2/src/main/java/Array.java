869f0f2567ace3160b484fb0e4eb93503781316c
ViktorKov <kovalskij8@yandex.ru>
2021-02-09 21:55:00 +0300

    ДЗ 2й урок

---
+++ Lesson2/src/main/java/Array.java
@@ -0,0 +1,29 @@
+public interface Array<E> {
+
+    void add(E value);
+
+    E get(int index);
+
+    boolean remove(E value);
+
+    E remove(int index);
+
+    int indexOf(E value);
+
+    boolean contains(E value);
+
+    boolean isEmpty();
+
+    int size();
+
+    void display();
+
+    void sortBubble();
+    void sortSelect();
+    void sortInsert();
+
+    E[] toArray();
+
+    Array<E> copy();
+
+}
---
+++ Lesson2/src/main/java/TestDZ.java
@@ -0,0 +1,68 @@
+import java.util.ArrayList;
+import java.util.Random;
+
+public class TestDZ {
+    private static final int ARRAY_CAPACITY = 100_000;
+    private static final int MAX_VALUE = 10_000;
+
+
+    public static void main(String[] args) {
+
+
+        Array<Integer> data1 = new ArrayImpl<>(ARRAY_CAPACITY);
+        Array<Integer> data2 = new ArrayImpl<>(ARRAY_CAPACITY);
+        Array<Integer> data3 = new ArrayImpl<>(ARRAY_CAPACITY);
+
+        addNumber(data1);
+        addNumber(data2);
+        addNumber(data3);
+
+        resultBubble(data1);
+        System.out.println("-----");
+        resultSelect(data2);
+        System.out.println("-----");
+        resultInsert(data3);
+
+
+
+    }
+
+    static void resultBubble (Array<Integer> data){
+        System.out.println("Started method: sortBubble");
+        long s = System.currentTimeMillis();
+        data.sortBubble();
+        long f = System.currentTimeMillis();
+        long res = f-s;
+        System.out.println("Finished method: sortBubble");
+        System.out.println("The time is: " + res);
+
+    }
+
+    static void resultSelect (Array<Integer> data){
+        System.out.println("Started method: sortSelect");
+        long s = System.currentTimeMillis();
+        data.sortSelect();
+        long f = System.currentTimeMillis();
+        long res = f-s;
+        System.out.println("Finished method: sortSelect");
+        System.out.println("The time is: " + res);
+    }
+
+    static void resultInsert (Array<Integer> data){
+        System.out.println("Started method: sortInsert");
+        long s = System.currentTimeMillis();
+        data.sortInsert();
+        long f = System.currentTimeMillis();
+        long res = f-s;
+        System.out.println("Finished method: sortInsert");
+        System.out.println("The time is: " + res);
+    }
+
+   static void addNumber(Array<Integer> data){
+       Random rand = new Random();
+       for (int i = 0; i < ARRAY_CAPACITY; i++) {
+           int value = rand.nextInt(MAX_VALUE);
+           data.add(value);
+       }
+   }
+}
