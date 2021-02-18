2c62f91ad447288e0b45e3788aa4951977354bc9
ViktorKov <kovalskij8@yandex.ru>
2021-02-18 14:46:49 +0300

    ДЗ 4й урок

---
+++ commonModul/src/main/java/Linkedlist/ListIterator.java
@@ -0,0 +1,13 @@
+package Linkedlist;
+
+import java.util.Iterator;
+
+public interface ListIterator<E> extends Iterator<E> {
+
+    void reset();
+
+    void insertBefore(E value);
+
+    void insertAfter(E value);
+
+}
