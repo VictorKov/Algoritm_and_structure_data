45a004a1b76ab3de6a77c9bf60c627a10154827b
ViktorKov <kovalskij8@yandex.ru>
2021-02-25 19:28:48 +0300

    6 урок

---
+++ commonModul/src/main/java/Tree/Tree.java
@@ -0,0 +1,26 @@
+package Tree;
+
+    public interface Tree<E extends Comparable<? super E>> {
+
+        enum TraverseMode {
+            IN_ORDER,
+            PRE_ORDER,
+            POST_ORDER,
+        }
+
+        void add(E value);
+
+        boolean contains(E value);
+
+        boolean remove(E value);
+
+        boolean isEmpty();
+
+        int size();
+
+        void display();
+
+        void traverse(TraverseMode mode);
+
+    }
+
